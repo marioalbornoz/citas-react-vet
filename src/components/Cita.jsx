3e2586238ffb578613b48cda8468da18891f6266
marioalbornoz <mario.albornozc@utem.cl>
2020-07-02 21:25:54 -0400

    Se agrega componente Cita

---
+++ src/components/Cita.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Cita = ({cita, eliminarCitas}) => (
+  <div className="cita">
+    <p>Mascota: <span>{cita.mascota}</span></p>
+    <p>Dueño: <span>{cita.propietario}</span></p>
+    <p>Fecha: <span>{cita.fecha}</span></p>
+    <p>Hora: <span>{cita.hora}</span></p>
+    <p>Sintomas: <span>{cita.sintomas}</span></p>
+
+    <button
+      className="button eliminar u-full-width"
+      onClick = {() => eliminarCitas(cita.id)}
+      >Eliminar &times;
+    </button>
+  </div>
+);
+ 
+export default Cita;
