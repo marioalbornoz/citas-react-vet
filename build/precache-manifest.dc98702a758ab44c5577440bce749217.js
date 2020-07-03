8b17e714f10b06d76a9ba3277f331623cb2d4020
marioalbornoz <mario.albornozc@utem.cl>
2020-07-02 21:56:25 -0400

    add build

---
+++ build/precache-manifest.dc98702a758ab44c5577440bce749217.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e7646c18b37528930c9c098389a2af4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43147ea7482bd8f6497d",
+    "url": "/static/css/main.3ec473bc.chunk.css"
+  },
+  {
+    "revision": "ff6c69bec84ba0fb62ea",
+    "url": "/static/js/2.d68e4dbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d68e4dbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43147ea7482bd8f6497d",
+    "url": "/static/js/main.ae510c21.chunk.js"
+  },
+  {
+    "revision": "1b13ee7f0369ce8df4c9",
+    "url": "/static/js/runtime-main.4dc01cc0.js"
+  }
+]);
