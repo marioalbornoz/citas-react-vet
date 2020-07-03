ebe819781c04106cb29c109f227137a4d97d6bd5
marioalbornoz <mario.albornozc@utem.cl>
2020-07-02 21:24:53 -0400

    - Se agrega eliminar a cada cita - Se añade localstorage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
+import Cita from './components/Cita';
 
 function App() {
-  // state arreglo de citas
-  const [citas, guardarCitas] = useState([]);
 
+
+  // Citas en localStorage
+
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if(!citasIniciales){
+    citasIniciales = [];
+  }
+
+  // state: arreglo de citas
+  const [citas, guardarCitas] = useState(citasIniciales); 
+
+  // UseEfect para realziar cierta operaciones cuando el state cambia
+  useEffect( () => {
+    if(citasIniciales){
+      localStorage.setItem('citas', JSON.stringify(citas));
+    } else {
+      localStorage.setItem('citas', JSON.stringify([]));
+    }
+  }, [citas]);
+
+  // Funcion que toma las cita actuales y agrega la nueva
   const crearCita = cita => {
     guardarCitas([
       ...citas,
       cita
     ]);
   }
+
+  //  Funcion que elimina una cita por su id
+  const eliminarCitas = id => {
+    console.log("Eliminando cita...");
+    const nuevoArrayCitas = citas.filter(cita => cita.id !== id)
+    guardarCitas(nuevoArrayCitas)
+  }
+
+  //  Mensaje condicional
+const titulo = citas.length === 0 ? 'No hay citas' : 'Administra tus citas'
+
   return (
     <Fragment>
       <h1>Administración de Pacientes</h1>
@@ -22,7 +53,15 @@ function App() {
               />
           </div>
           <div className="one-half column">
-              2
+            <h2>{titulo}</h2>
+            {
+              citas.map(cita =>(
+                <Cita
+                  key={cita.id}
+                  cita = {cita}
+                  eliminarCitas = {eliminarCitas}
+                />
+              ))}
           </div>
         </div>
       </div>
